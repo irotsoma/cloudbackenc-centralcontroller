63478b96a171aedb235887bdbeebf72b3d69096e
irotsoma <irotsomadev@gmail.com>
2017-03-24 12:19:56 -0700

    Updated copyright notification.

--- src/main/kotlin/com/irotsoma/cloudbackenc/centralcontroller/controllers/CloudServiceLoginController.kt
+++ src/main/kotlin/com/irotsoma/cloudbackenc/centralcontroller/controllers/CloudServiceLoginController.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016  Irotsoma, LLC
+ * Copyright (C) 2017  Irotsoma, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -10,7 +10,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
