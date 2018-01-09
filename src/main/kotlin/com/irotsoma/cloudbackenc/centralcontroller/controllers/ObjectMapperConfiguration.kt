1d9bbb272fe31db88f51091ee25d0ce57f612df4
irotsoma <irotsomadev@gmail.com>
2018-01-09 15:16:49 -0800

    Update copyright notices

--- src/main/kotlin/com/irotsoma/cloudbackenc/centralcontroller/controllers/ObjectMapperConfiguration.kt
+++ src/main/kotlin/com/irotsoma/cloudbackenc/centralcontroller/controllers/ObjectMapperConfiguration.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2017  Irotsoma, LLC
+ * Copyright (C) 2016-2018  Irotsoma, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
