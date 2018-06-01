1b0b723a0d42e8cb79a66cc2427eab54ad96b4b0
irotsoma <irotsomadev@gmail.com>
2018-06-01 09:42:18 -0700

    Setting version to 0.3 as an alpha milestone.

--- src/main/kotlin/com/irotsoma/cloudbackenc/centralcontroller/Startup.kt
+++ src/main/kotlin/com/irotsoma/cloudbackenc/centralcontroller/Startup.kt
@@ -21,6 +21,10 @@ package com.irotsoma.cloudbackenc.centralcontroller
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
 
 /**
  * Primary class
