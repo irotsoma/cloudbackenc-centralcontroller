3236d0308e1cf2761403af4c1067061ea9544420
irotsoma <irotsomadev@gmail.com>
2019-10-25 13:50:17 -0700

    Changed common library to use a differently named messages.properties, so it works better with calling apps.

--- src/main/kotlin/com/irotsoma/cloudbackenc/centralcontroller/CustomWebMvcConfigurerAdapter.kt
+++ src/main/kotlin/com/irotsoma/cloudbackenc/centralcontroller/CustomWebMvcConfigurerAdapter.kt
@@ -18,14 +18,17 @@
  */
 package com.irotsoma.cloudbackenc.centralcontroller
 
+import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.ResourceBundleMessageSource
 import org.springframework.web.servlet.LocaleResolver
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor
 import org.springframework.web.servlet.i18n.SessionLocaleResolver
 import java.util.*
+
 
 /**
  * Configuration to allow for the application to use a session based locale resolver
@@ -54,5 +57,14 @@ class CustomWebMvcConfigurerAdapter : WebMvcConfigurer {
         if (registry != null) {
             super.addInterceptors(registry)
         }
+    }
+
+    @Bean
+    fun messageSource(): MessageSource {
+        val source = ResourceBundleMessageSource()
+        source.setBasenames("classpath:messages",
+                            "classpath:common-messages")
+        source.setDefaultEncoding("UTF-8")
+        return source
     }
 }
